2d22fe62a188f887412c8cc0e046b9fbcd7619f0
grasshopper7 <grass.hopper.moc@gmail.com>
2021-12-31 14:44:11 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Spark Extent Report for REST API testing with REST Assured using JUnit 5
+
+Refer to this [article](https://ghchirp.tech/3791/) for more details.
---
+++ src/test/java/endpoints/NestedCityAirDataIT.java
@@ -0,0 +1,30 @@
+package endpoints;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import io.qameta.allure.restassured.AllureRestAssured;
+
+public class NestedCityAirDataIT {
+
+	private static final String token = "1a5d8f027e344fa191b88e966bdb5d4aa4853d4a";
+
+	@Test
+	public void getRomeAirData() {
+		given().param("token", token).filter(new AllureRestAssured()).when().get("https://api.waqi.info/feed/rome/")
+				.then().statusCode(equalTo(200));
+	}
+
+	@Nested
+	class Tabbed {
+
+		@Test
+		public void getParisAirData() {
+			given().param("token", token).filter(new AllureRestAssured()).when()
+					.get("https://api.waqi.info/feed/paris/").then().statusCode(equalTo(200));
+		}
+	}
+}
